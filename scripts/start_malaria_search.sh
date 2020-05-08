5d888f7d50835d98427f3771b8e41457739498dc
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-05-08 13:40:05 +0900

    search s3 dp0 wd 0.0003 script added

--- scripts/start_malaria_search.sh
+++ scripts/start_malaria_search.sh
@@ -9,5 +9,5 @@ source /vol/cuda/10.0.130/setup.sh
 TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
-python src/search/train_search.py --unrolled --cutout --report_freq_hessian 2 --space s1 --dataset malaria --drop_path_prob 0.0 --weight_decay 0.0027 --batch_size 16
+python src/search/train_search.py --unrolled --cutout --report_freq_hessian 2 --space s3 --dataset malaria --drop_path_prob 0.0 --weight_decay 0.0003 --batch_size 16
 
