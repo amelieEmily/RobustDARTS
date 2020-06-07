44d077b1c0f3ce070ae3f67ff2f283469b40927b
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-06-08 00:55:10 +0900

    making dr detection model not have 2 meuron phase right before final classification layer

--- src/evaluation/train.py
+++ src/evaluation/train.py
@@ -87,8 +87,7 @@ def main():
   darts_model = Network(args.init_channels, args.n_classes, args.layers, args.auxiliary, genotype)
 
   if args.dataset == 'dr-detection':
-    extension = NetworkExtension(10, 5, args.auxiliary)
-    model = nn.Sequential(darts_model, extension)
+    model = darts_model
   else:
     model = darts_model
   if TORCH_VERSION.startswith('1'):
