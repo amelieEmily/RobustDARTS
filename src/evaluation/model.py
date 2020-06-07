8428854657613ecd1b4cc08f6d77be4ae9322eb9
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-06-08 01:18:56 +0900

    restored the aux

--- src/evaluation/model.py
+++ src/evaluation/model.py
@@ -67,30 +67,17 @@ class AuxiliaryHead(nn.Module):
   def __init__(self, C, num_classes):
     """assuming input size 8x8"""
     super(AuxiliaryHead, self).__init__()
-    # self.features = nn.Sequential(
-    #   nn.ReLU(inplace=True),
-    #   nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
-    #   nn.Conv2d(C, 128, 1, bias=False),
-    #   nn.BatchNorm2d(128),
-    #   nn.ReLU(inplace=True),
-    #   nn.Conv2d(128, 768, 2, bias=False),
-    #   nn.BatchNorm2d(768),
-    #   nn.ReLU(inplace=True)
-    # ) aux for other dataset beneath is for dr-detection
     self.features = nn.Sequential(
-        nn.ReLU(inplace=True),
-        nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
-        nn.Conv2d(C, 128, 3, bias=False),
-        nn.BatchNorm2d(128),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(128, 768, 3, bias=False),
-        nn.BatchNorm2d(768),
-        nn.ReLU(inplace=True),
-
-        nn.Conv2d(768, 1, 1, bias=False),
-        nn.ReLU(inplace=True),
-
-      )
+      nn.ReLU(inplace=True),
+      nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
+      nn.Conv2d(C, 128, 1, bias=False),
+      nn.BatchNorm2d(128),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(128, 768, 2, bias=False),
+      nn.BatchNorm2d(768),
+      nn.ReLU(inplace=True)
+    )
+    #aux for other dataset beneath is for dr-detection
     # for cifar1, cifar100, svhn:
     # self.classifier = nn.Linear(768, num_classes)
 
