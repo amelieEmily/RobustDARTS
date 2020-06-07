31c2dc61d0a4e288ddc5cfd9ad565696b39be364
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-06-08 01:31:32 +0900

    fixed aux so I don't have to hardcode everytime

--- src/evaluation/model.py
+++ src/evaluation/model.py
@@ -67,30 +67,36 @@ class AuxiliaryHead(nn.Module):
   def __init__(self, C, num_classes):
     """assuming input size 8x8"""
     super(AuxiliaryHead, self).__init__()
-    # self.features = nn.Sequential(
-    #   nn.ReLU(inplace=True),
-    #   nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
-    #   nn.Conv2d(C, 128, 1, bias=False),
-    #   nn.BatchNorm2d(128),
-    #   nn.ReLU(inplace=True),
-    #   nn.Conv2d(128, 768, 2, bias=False),
-    #   nn.BatchNorm2d(768),
-    #   nn.ReLU(inplace=True)
-    # ) aux for other dataset beneath is for dr-detection
-    self.features = nn.Sequential(
+    if num_classes == 5 : #if data is dr
+      self.features = nn.Sequential(
+          nn.ReLU(inplace=True),
+          nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
+          nn.Conv2d(C, 128, 3, bias=False),
+          nn.BatchNorm2d(128),
+          nn.ReLU(inplace=True),
+          nn.Conv2d(128, 768, 3, bias=False),
+          nn.BatchNorm2d(768),
+          nn.ReLU(inplace=True),
+
+          nn.Conv2d(768, 1, 1, bias=False),
+          nn.ReLU(inplace=True),
+
+        )
+      self.classifier = nn.Linear(1444, num_classes)
+    else:
+
+      self.features = nn.Sequential(
         nn.ReLU(inplace=True),
         nn.AvgPool2d(5, stride=3, padding=0, count_include_pad=False), # image size = 2 x 2
-        nn.Conv2d(C, 128, 3, bias=False),
+        nn.Conv2d(C, 128, 1, bias=False),
         nn.BatchNorm2d(128),
         nn.ReLU(inplace=True),
-        nn.Conv2d(128, 768, 3, bias=False),
+        nn.Conv2d(128, 768, 2, bias=False),
         nn.BatchNorm2d(768),
-        nn.ReLU(inplace=True),
-
-        nn.Conv2d(768, 1, 1, bias=False),
-        nn.ReLU(inplace=True),
-
+        nn.ReLU(inplace=True)
       )
+      self.classifier = nn.Linear(1444, num_classes)
+
     # for cifar1, cifar100, svhn:
     # self.classifier = nn.Linear(768, num_classes)
 
@@ -98,7 +104,6 @@ class AuxiliaryHead(nn.Module):
     #self.classifier = nn.Linear(221952, num_classes)
 
     # for malaria:
-    self.classifier = nn.Linear(1444, num_classes)
   def forward(self, x):
     x = self.features(x)
     x = self.classifier(x.view(x.size(0), -1))
