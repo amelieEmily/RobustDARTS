9b464bd094e38c32e92daec80fafd532a892d409
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-06-07 23:37:53 +0900

    making dr-detection use cells found from malaria data

--- src/evaluation/train.py
+++ src/evaluation/train.py
@@ -73,7 +73,7 @@ def main():
 
   # load search configuration file holding the found architectures
   if args.dataset == 'dr-detection':
-    configuration = '_'.join([args.space, 'cifar10']) #transer learning from cifar10
+    configuration = '_'.join([args.space, 'malaria']) #transer learning from cifar10
   else:
     configuration = '_'.join([args.space, args.dataset])
   settings\
