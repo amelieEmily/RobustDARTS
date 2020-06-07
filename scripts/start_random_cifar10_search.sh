6c31644faba8754321519a9664c5066ceec2a3d9
rx5935 <yejinseo@anotherui-MacBook-Air.local>
2020-06-07 15:08:53 +0900

    added random search on cifar 10 wd 0.0003 dp0

--- scripts/start_random_cifar10_search.sh
+++ scripts/start_random_cifar10_search.sh
@@ -10,4 +10,4 @@ TERM=vt100 # or TERM=xterm
 /usr/bin/nvidia-smi
 uptime
 
-src/search/randomNAS/random_weight_share.py --seed 10 --epochs 50 --save experiments/search_logs_RandomNAS --space s3 --dataset cifar10 --drop_path_prob 0 --weight_decay 0.0003
+python src/search/randomNAS/random_weight_share.py --seed 10 --epochs 50 --save experiments/search_logs_RandomNAS --space s3 --dataset cifar10 --drop_path_prob 0 --weight_decay 0.0003
